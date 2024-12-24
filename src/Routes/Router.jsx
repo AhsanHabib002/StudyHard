83772391d0a02e46040fd07e7cf690386085100b
Your NameAhsanHabib002 <ahsanhabib12300@gmail.com>
2024-12-24 15:26:17 +0600

    Route and page setup

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import Mainlayout from "../Layout/Mainlayout";
+import Home from "../Pages/Home";
 
 const Router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Mainlayout></Mainlayout>,
+    errorElement: <h2>Route not Found</h2>,
+    children:[
+        {
+            path:"/",
+            element: <Home></Home>
+        }
+    ]
   },
 ]);
 
