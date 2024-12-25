db4883bf011a3964416cc6aeaaa2b3857a1806df
Your NameAhsanHabib002 <ahsanhabib12300@gmail.com>
2024-12-25 22:55:15 +0600

    login setup done

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AuthContext from "./AuthContext";
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   signInWithPopup,
   signOut,
   updateProfile,
 } from "firebase/auth";
 import auth from "../../firebase/firebase.init";
-import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import { GoogleAuthProvider } from "firebase/auth";
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
@@ -32,8 +33,16 @@ const AuthProvider = ({ children }) => {
   const googleProvider = new GoogleAuthProvider();
   const googleLogin = () => {
     return signInWithPopup(auth, googleProvider);
-};
-
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
+      setUser(currentuser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   const authInfo = {
     user,
     setUser,
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,130 @@
-import React from 'react';
+import { useContext, useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Swal from "sweetalert2";
+import AuthContext from "../../Context/AuthContext/AuthContext";
 
 const Login = () => {
-    return (
-        <div className="max-w-[90rem] mx-auto">
-            this is login
+  const { userLogin, setUser, googleLogin } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const name = form.get("name");
+    const email = form.get("email");
+    const photo = form.get("photo");
+    const password = form.get("password");
+    userLogin(email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        setTimeout(() => {
+          navigate("/");
+        }, 2000);
+        Swal.fire("Logger In", "Your Login is Successful.", "success");
+      })
+      .catch((error) => {
+        toast.error(`Error: ${error.message}`);
+      });
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+    if (!passwordRegex.test(password)) {
+      setError(
+        "Password must contain at least 6 characters, one uppercase letter, and one lowercase letter."
+      );
+      return;
+    }
+    setError("");
+  };
+
+  const handleGoogleLogin = () => {
+    googleLogin()
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        setTimeout(() => {
+          navigate("/");
+        }, 2000);
+        toast.success("Login Successful");
+      })
+      .catch((error) => {
+        toast.error(`Error: ${error.message}`);
+      });
+  };
+
+  return (
+    <div className="max-w-[90rem] mx-auto">
+      <div className="max-w-[450px] mx-auto py-[90px] px-2">
+        <ToastContainer />
+        <div className="card bg-base-100 w-full  shrink-0 shadow-2xl">
+          <form onSubmit={handleLogin} className="card-body">
+            <h2 className="text-center font-semibold text-2xl mb-4">
+              Login Your Account
+            </h2>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control relative">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type={show ? "text" : "password"}
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <button
+                onClick={() => setShow(!show)}
+                className="btn btn-xs absolute right-4 top-12"
+              >
+                <FaEye></FaEye>
+              </button>
+              <label className="label">
+                <button className="label-text-alt link link-hover">
+                  {" "}
+                  Forget password?
+                </button>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn bg-[#B9FF66]">Login</button>
+            </div>
+          </form>
+          <div className="px-8 w-full">
+            <div className="divider"></div>
+            <button 
+            onClick={handleGoogleLogin}
+            className="btn btn-outline w-full">
+              Login With Google
+            </button>
+
+            <div className="flex gap-2 mt-4 text-sm  mb-6">
+              <p>Don't have an account?</p>
+              <div className="font-semibold text-[#6980ff] ">
+                <Link to="/register">Register Now</Link>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
